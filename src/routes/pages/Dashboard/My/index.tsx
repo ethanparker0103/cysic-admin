4e396b299a5bf0f483e4353f1b6685a7a90c8295
Eden <eden.cc@outlook.com>
2024-09-30 10:18:19 +0800

    feat

--- src/routes/pages/Dashboard/My/index.tsx
+++ src/routes/pages/Dashboard/My/index.tsx
@@ -1,3 +1,4 @@
+import MainCard from "@/components/MainCard";
 import MainContainer from "@/routes/pages/Dashboard/components/mainContainer";
 import Detail from "@/routes/pages/Dashboard/My/Detail";
 
@@ -17,33 +18,6 @@ const removeNegativeZero = (str: string) => {
 const format = (value: string | number, defaultValue = undefined) => {
   if (!value) return defaultValue;
   return removeNegativeZero(BigNumber(value).toFormat());
-};
-
-const MainCard = (props: any) => {
-  const { t } = useTranslation();
-  return (
-    <div
-      className={clsx(
-        isMobile ? "w-full px-5 py-4" : "min-h-[152px] flex-1 px-6 py-6",
-        " flex flex-col gap-3 rounded-[24px] border border-[#192E33] shadow-[0px_4px_0px_0px_#000000]"
-      )}
-      style={{
-        backgroundColor: "#10141a",
-        // background: `url('/m/btn-bg.svg')`,
-        // backgroundRepeat: "no-repeat",
-        // backgroundSize: "auto",
-      }}
-      {...props}
-    >
-      <div className="flex items-center gap-1 text-[#A3A3A3]">
-        {/* <Hero className="size-4 text-[#A3A3A3]" /> */}
-        <span className="text-[18px] text-[#fff]">
-          {typeof props?.title == "string" ? t(props?.title) : props?.title}
-        </span>
-      </div>
-      <div className="text-[24px] text-[#fff] font-bold">{props?.children}</div>
-    </div>
-  );
 };
 
 // {
