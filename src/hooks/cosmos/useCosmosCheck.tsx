445c63225a606c71f3ceb6808c47351e7acf20da
Eden <eden.cc@outlook.com>
2024-11-03 03:57:45 +0800

    feat

--- src/hooks/cosmos/useCosmosCheck.tsx
+++ src/hooks/cosmos/useCosmosCheck.tsx
@@ -2,6 +2,7 @@ import useAccount from "@/hooks/useAccount";
 import useCosmos from "@/models/_global/cosmos";
 import { convertAddrByProvider } from "@/utils/cosmos";
 import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 
 const useCosmosCheck = () => {
@@ -14,6 +15,7 @@ const useCosmosCheck = () => {
                 console.log('ethereumHexAddress', ethereumHexAddress, bech32Address)
                 if(cosmosAddr?.toLowerCase() !== bech32Address?.toLowerCase() || ethereumHexAddress?.toLowerCase() !== evmAddr?.toLowerCase()){
 
+                    toast.error(`Looks like your walletaddress doesn't match. Connect the right EVM address to keep going.`)
                     dispatchEvent(new CustomEvent('modal_download_keplr_visible', {detail:{
                         type: 'unmathedAddress',
                         visible: true
