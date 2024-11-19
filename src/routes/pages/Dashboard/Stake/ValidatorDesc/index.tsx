9eab7b523907f58f41e9e6a665c91bbd6ca0b71f
Eden <eden.cc@outlook.com>
2024-11-19 09:51:39 +0800

    feat

--- src/routes/pages/Dashboard/Stake/ValidatorDesc/index.tsx
+++ src/routes/pages/Dashboard/Stake/ValidatorDesc/index.tsx
@@ -3,16 +3,14 @@ const ValidatorDesc = () => {
     <div className="flex flex-col gap-6 border border-[#FFFFFF33] rounded-[16px]  p-8">
       <div className="flex items-center gap-3">
         <span className="text-lg font-[500]">Your Validators</span>
-        <span className="text-sm font-[400]">APR 15.97%</span>
+        {/* <span className="text-sm font-[400]">APR 15.97%</span> */}
       </div>
       <div className="flex flex-col gap-1">
         <div className="font-[500]">
           ✨ Wanna grow your assets? Start staking!
         </div>
         <div className=" text-[#A3A3A3] text-sm font-[400]">
-          In Proof-of-Stake networks, staking your assets with validators is
-          essential to earn voting power and staking rewards.To start staking,
-          first deposit or buy CYSIC to your address and click on Stake.
+          In Proof-of-Stake networks, staking your assets with validators is essential to earn voting power and securing the network. To start staking, first deposit or buy CYS to your address and click on Stake.
         </div>
       </div>
     </div>
