49ab4ec45b11b6045eff6557bfc7008898bc1543
Eden <eden.cc@outlook.com>
2024-11-19 20:20:52 +0800

    fix

--- src/routes/pages/Dashboard/Stake/ValidatorDesc/index.tsx
+++ src/routes/pages/Dashboard/Stake/ValidatorDesc/index.tsx
@@ -7,10 +7,10 @@ const ValidatorDesc = () => {
       </div>
       <div className="flex flex-col gap-1">
         <div className="font-[500]">
-          ✨ Wanna grow your assets? Start staking!
+          ✨ Want to grow your assets? Start staking today!
         </div>
         <div className=" text-[#A3A3A3] text-sm font-[400]">
-          In Proof-of-Stake networks, staking your assets with validators is essential to earn voting power and securing the network. To start staking, first deposit or buy CYS to your address and click on Stake.
+          Staking your assets with validators is a great way to earn rewards while helping to secure the network and gain voting power. Simply stake your <b>$CGT</b> to start earning <b>$CYS</b> as rewards. Getting started is easy—just select your preferred validator and begin staking!
         </div>
       </div>
     </div>
