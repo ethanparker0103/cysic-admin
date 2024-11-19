0472019923bb76d7c731c7debaf1f296b3969aa4
Eden <eden.cc@outlook.com>
2024-11-19 20:21:23 +0800

    fix

--- src/routes/pages/Dashboard/Stake/ValidatorDesc/index.tsx
+++ src/routes/pages/Dashboard/Stake/ValidatorDesc/index.tsx
@@ -10,7 +10,7 @@ const ValidatorDesc = () => {
           ✨ Want to grow your assets? Start staking today!
         </div>
         <div className=" text-[#A3A3A3] text-sm font-[400]">
-          Staking your assets with validators is a great way to earn rewards while helping to secure the network and gain voting power. Simply stake your <b>$CGT</b> to start earning <b>$CYS</b> as rewards. Getting started is easy—just select your preferred validator and begin staking!
+          Staking your assets with validators is a great way to earn rewards while helping to secure the network and gain voting power. Simply stake your <b>$CGT</b> to start earning <b>$CYS</b> as rewards. Getting started is easy—just click on stake, select your preferred validator, enter your staking amount, and begin staking!
         </div>
       </div>
     </div>
