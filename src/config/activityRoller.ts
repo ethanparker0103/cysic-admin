f077eb64e5ed99ac2f353f9447007a3f6e034800
Eden <eden.cc@outlook.com>
2024-11-26 23:23:43 +0800

    feat

--- src/config/activityRoller.ts
+++ src/config/activityRoller.ts
@@ -1,5 +1,9 @@
 export const activityRoller = [
     {
+        content: 'Phase 2 is now active!',
+        action: ()=>dispatchEvent(new CustomEvent('modal_phase_2_desc_visible', { detail: {visible: true} }))
+    },
+    {
         content: "Update your verifier program now if you haven't (Nov 22nd Version)",
         action: ()=>window.open('https://x.com/CysicCommunity/status/1861404452806369762', '_blank')
     },
