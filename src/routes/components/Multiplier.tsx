e5c6f058c13159446501c01eea7b82471e17fcf7
Ethan <ethanparker0103@outlook.com>
2025-05-16 08:24:08 +0800

    feat

--- src/routes/components/Multiplier.tsx
+++ src/routes/components/Multiplier.tsx
@@ -11,40 +11,42 @@ export const Multiplier = () => {
     return (
         <GradientBorderCard borderRadius={8} className="flex-[3] w-full">
             <div className="flex flex-col ">
-            <div className="w-full px-6 py-4 h-full flex flex-col justify-between gap-6">
-                <div className="flex items-center gap-1">
-                    <div className="uppercase !text-base !font-light title">
-                        MULTIPLIER
+                <div className="w-full px-6 py-4 h-full flex flex-col justify-between gap-6">
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center gap-1">
+                            <div className="uppercase !text-base !font-light title">
+                                MULTIPLIER
+                            </div>
+                            <Tooltip content={<div className="max-w-[16rem] py-2">A higher level Multiplier gives you a boost in rewards when earning rewards through Verifier/Prover.</div>}><div className="text-sub text-xs">ⓘ</div></Tooltip>
+                        </div>
+                        <div
+                            className="flex items-center justify-end gap-1 cursor-pointer"
+                            onClick={handleMultiplierModal}
+                        >
+                            <span className="text-sm text-sub text-sub">SPEED UP</span>
+                            <ArrowRight size={12} />
+                        </div>
                     </div>
-                    <Tooltip content={<div className="max-w-[16rem] py-2">A higher level Multiplier gives you a boost in rewards when earning rewards through Verifier/Prover.</div>}><div className="text-sub text-xs">ⓘ</div></Tooltip>
-                </div>
 
-                {/* 进度条 */}
-                <div className="relative w-full h-2 bg-gray-500 rounded-full overflow-hidden">
-                    <div 
-                        style={{ width: `${multiplierPercent}%` }}
-                        className="absolute inset-0 left-0 h-full bg-gradient-to-r from-purple-500 via-blue-400 to-green-300 rounded-full"
-                    ></div>
-                </div>
+                    {/* 进度条 */}
+                    <div className="relative w-full h-2 bg-gray-500 rounded-full overflow-hidden">
+                        <div
+                            style={{ width: `${multiplierPercent}%` }}
+                            className="absolute inset-0 left-0 h-full bg-gradient-to-r from-purple-500 via-blue-400 to-green-300 rounded-full"
+                        ></div>
+                    </div>
 
-                {/* HIGH SPEED 指示器 */}
-                <div
-                    className={cn(
-                        "flex items-center gap-2",
-                        isMobile ? "self-start" : "self-end"
-                    )}
-                >
-                    <div className="w-3 h-3 rounded-full bg-blue-400"></div>
-                    <span className="!text-sm title">HIGH SPEED</span>
+                    {/* HIGH SPEED 指示器 */}
+                    <div
+                        className={cn(
+                            "flex items-center gap-2",
+                            isMobile ? "self-start" : "self-end"
+                        )}
+                    >
+                        <div className="w-3 h-3 rounded-full bg-blue-400"></div>
+                        <span className="!text-sm title">HIGH SPEED</span>
+                    </div>
                 </div>
-            </div>
-            <div
-                className="flex items-center justify-end gap-1 cursor-pointer px-6 pb-4"
-                onClick={handleMultiplierModal}
-            >
-                <span className="text-sm text-sub text-sub">SPEED UP</span>
-                <ArrowRight size={12} />
-            </div>
             </div>
         </GradientBorderCard>
     );
