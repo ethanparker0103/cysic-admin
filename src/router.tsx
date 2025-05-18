c77abdee37663f9a0cc131f4f1e73af4e47b3ca3
Ethan <ethanparker0103@outlook.com>
2025-05-18 11:32:51 +0800

    feat

--- src/router.tsx
+++ src/router.tsx
@@ -327,7 +327,8 @@ export const backgroundImageList = {
     img: getImageUrl('@/assets/images/_global/userPortal_landing_bg.png'),
     className: "h-screen green-landing",
     style: {
-      backgroundPosition: "center -10vh"
+      backgroundPosition: "center -10vh",
+      filter: "sepia(1) hue-rotate(90deg) brightness(0.3)"
     },
     needShadow: true
   },
@@ -335,7 +336,8 @@ export const backgroundImageList = {
     img: getImageUrl('@/assets/images/_global/userPortal_landing_bg.png'),
     className: "h-screen green-landing",
     style: {
-      backgroundPosition: "center -10vh"
+      backgroundPosition: "center -10vh",
+      filter: "sepia(1) hue-rotate(90deg) brightness(0.3)"
     },
     needShadow: true
   },
