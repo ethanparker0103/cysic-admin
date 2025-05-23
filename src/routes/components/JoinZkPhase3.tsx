da6ac410a99306914bfbe5ed65f3ee18d3ff6484
ethanparker <ethanparker0103@outlook.com>
2025-05-23 10:11:19 +0800

    fix

--- src/routes/components/JoinZkPhase3.tsx
+++ src/routes/components/JoinZkPhase3.tsx
@@ -6,7 +6,7 @@ const JoinZkPhase3 = ({ slogen, title = 'Phase III', subTitle, className }: { sl
     return (
         <div className={cn("relative z-1 size-full  flex flex-col gap-4 items-center justify-center", className)}>
             <span className={cn('slogen', classes.subTitle)}>{slogen}</span>
-            <span className="unbounded text-[48px] lg:text-[11.25rem] font-[200] text-center">{title}</span>
+            <span className="unbounded text-[48px] lg:text-[6rem] font-[200] text-center">{title}</span>
             {subTitle && <span className={cn('subTitle text-center', classes.subTitle)}>{subTitle}</span>}
             <div className="flex flex-col lg:flex-row items-center gap-6">
                 <JoinTestnetPhaseIIIButton />
