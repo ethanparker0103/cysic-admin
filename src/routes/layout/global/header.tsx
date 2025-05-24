75189d82890a9a03852083a187ecb2bd6a5bc481
ethanparker <ethanparker0103@outlook.com>
2025-05-24 14:14:18 +0800

    feat

--- src/routes/layout/global/header.tsx
+++ src/routes/layout/global/header.tsx
@@ -1,137 +1,203 @@
 import GradientBorderCard from "@/components/GradientBorderCard";
 import GradientNavDropdown from "@/components/GradientNavDropdown";
 import { BIND_CHECK_PATHS, NO_BIND_CHECK_PATHS } from "@/config";
-import { getImageUrl, handleLoginPersonalMessage, handleSignIn } from "@/utils/tools";
+import {
+  getImageUrl,
+  handleLoginPersonalMessage,
+  handleSignIn,
+} from "@/utils/tools";
 
-import { ArrowRight, Menu } from 'lucide-react';
+import { ArrowRight, Menu } from "lucide-react";
 import ConnectInfo from "@/components/ConnectInfo";
 import useAccount from "@/hooks/useAccount";
 import useNav from "@/hooks/useNav";
 import { Link, useLocation } from "react-router-dom";
 import { useMemo } from "react";
-import { isMobile } from "react-device-detect";
 import { cn, Drawer, DrawerContent, useDisclosure } from "@nextui-org/react";
 import Button from "@/components/Button";
 import { createPortal } from "react-dom";
 import useTriedConnectedOnce from "@/hooks/useTriedConnectedOnce";
 import Spinner from "@/components/spinner";
 
-const size = 'full'
+const size = "full";
 
 export default function Header() {
-    const { walletAddress, isSigned, isBinded } = useAccount();
-    const { currentNavs } = useNav();
-    const location = useLocation();
+  const { walletAddress, isSigned, isBinded } = useAccount();
+  const { currentNavs } = useNav();
+  const location = useLocation();
 
-    const { hasTryToConnectedOnce } = useTriedConnectedOnce();
+  const { hasTryToConnectedOnce } = useTriedConnectedOnce();
 
-    // 检查当前路径是否需要绑定邀请码
-    const needsBindCheck = useMemo(() => {
-        const path = location.pathname;
+  // 检查当前路径是否需要绑定邀请码
+  const needsBindCheck = useMemo(() => {
+    const path = location.pathname;
 
-        return BIND_CHECK_PATHS.some(checkPath => path.includes(checkPath) && !NO_BIND_CHECK_PATHS.includes(path));
-    }, [location.pathname]);
+    return BIND_CHECK_PATHS.some(
+      (checkPath) =>
+        path.includes(checkPath) && !NO_BIND_CHECK_PATHS.includes(path)
+    );
+  }, [location.pathname]);
 
-    // 需要显示绑定提示的条件
-    const shouldShowBindPrompt = isSigned && needsBindCheck && !isBinded;
+  // 需要显示绑定提示的条件
+  const shouldShowBindPrompt = isSigned && needsBindCheck && !isBinded;
 
+  const handleConnect = () => {
+    handleSignIn();
+  };
 
-    const handleConnect = () => {
-        handleSignIn();
-    };
+  const handleBindInviteCode = () => {
+    handleSignIn();
+  };
 
-    const handleBindInviteCode = () => {
-        handleSignIn();
-    };
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleOpen = () => {
+    onOpen();
+  };
 
-    const handleOpen = () => {
-        onOpen();
-    };
+  return (
+    <>
+      <>
+        <div className="lg:hidden h-[5.625rem] flex items-center relative z-[1]">
+          <div className="flex flex-wrap gap-3 relative w-full">
+            <Button onClick={handleOpen}>
+              <Menu width={30} height={30} />
+            </Button>
 
-    return isMobile ? <>
-        <div className="h-[5.625rem] flex items-center relative z-[1]">
-            <div className="flex flex-wrap gap-3 relative w-full">
-                <Button onClick={handleOpen}>
-                    <Menu width={30} height={30} />
-                </Button>
+            <Link
+              to={"/"}
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            >
+              <img
+                src={getImageUrl("@/assets/images/logo/cysic.svg")}
+                className="w-[11.25rem]"
+              />
+            </Link>
+          </div>
+          <Drawer isOpen={isOpen} size={size} onClose={onClose}>
+            <DrawerContent className="bg-[#090A09]">
+              {(onClose) => (
+                <>
+                  <div className="h-[5.625rem] flex flex-wrap gap-3 relative w-full -z-[1]">
+                    <Link
+                      to={"/"}
+                      className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                    >
+                      <img
+                        src={getImageUrl("@/assets/images/logo/cysic.svg")}
+                        className="w-[11.25rem]"
+                      />
+                    </Link>
+                  </div>
 
-                <Link to={'/'} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" >
-                    <img src={getImageUrl('@/assets/images/logo/cysic.svg')} className="w-[11.25rem]" />
-                </Link>
-            </div>
-            <Drawer isOpen={isOpen} size={size} onClose={onClose}>
-                <DrawerContent className="bg-[#090A09]">
-                    {(onClose) => (
-                        <>
-                            <div className="h-[5.625rem] flex flex-wrap gap-3 relative w-full -z-[1]">
-                                <Link to={'/'} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" >
-                                    <img src={getImageUrl('@/assets/images/logo/cysic.svg')} className="w-[11.25rem]" />
-                                </Link>
-                            </div>
-
-                            <div className="flex flex-col items-center gap-10 overflow-y-auto py-12">
-                                {currentNavs?.map((i: any) => {
-                                    if (i?.children?.length) {
-                                        return i?.children?.map((j: any) => {
-                                            return (<Link onClick={onClose} to={j?.href} className={cn("text-xl !text-sub", j?.disabled && "blur-[5px]")}>{j.content}</Link>)
-                                        })
-                                    } else {
-                                        return (<Link onClick={onClose} to={i?.href || '/'} className={cn("text-xl !text-sub", i?.disabled && "blur-[5px]")}>{i.content}</Link>)
-                                    }
-                                })}
-                            </div>
-
-
-                        </>
-                    )}
-                </DrawerContent>
-            </Drawer>
+                  <div className="flex flex-col items-center gap-10 overflow-y-auto py-12">
+                    {currentNavs?.map((i: any) => {
+                      if (i?.children?.length) {
+                        return i?.children?.map((j: any) => {
+                          return (
+                            <Link
+                              onClick={onClose}
+                              to={j?.href}
+                              className={cn(
+                                "text-xl !text-sub",
+                                j?.disabled && "blur-[5px]"
+                              )}
+                            >
+                              {j.content}
+                            </Link>
+                          );
+                        });
+                      } else {
+                        return (
+                          <Link
+                            onClick={onClose}
+                            to={i?.href || "/"}
+                            className={cn(
+                              "text-xl !text-sub",
+                              i?.disabled && "blur-[5px]"
+                            )}
+                          >
+                            {i.content}
+                          </Link>
+                        );
+                      }
+                    })}
+                  </div>
+                </>
+              )}
+            </DrawerContent>
+          </Drawer>
         </div>
-    </> : <>
-        {
-            createPortal(
-                <div className="main-container fixed top-0 z-[11] h-[8rem] w-full left-1/2 -translate-x-1/2">
-                    <div className="relative py-6">
-                        <GradientBorderCard className="h-20 flex items-center backdrop-blur bg-[#090A09B2]">
-                            <div className="w-full h-full flex justify-between items-center">
-                                <div className="flex items-center h-full flex-1">
-                                    <Link to={'/'}><img src={getImageUrl('@/assets/images/logo/cysic.svg')} className="flex-1 max-w-[11.25rem]" /></Link>
+      </>
 
-                                    {currentNavs.map((nav: any) => (
-                                        <GradientNavDropdown className="flex-1 max-w-[11.25rem] text-center" key={nav.content} item={nav} />
-                                    ))}
-                                </div>
-                                <div className="h-full flex items-center justify-end w-[26.75rem]">
-                                    {!hasTryToConnectedOnce ? <Spinner className="px-10" /> : walletAddress ? (
-                                        !isSigned ? (
-                                            <div onClick={handleLoginPersonalMessage} className="px-10 w-fit h-full flex items-center justify-end gap-1 cursor-pointer hover:bg-gradient-to-r from-[#17D1B2] to-[#4C1F99] ">
-                                                <span className="text-sub font-[400] uppercase text-sm">SIGN MESSAGE</span>
-                                                <ArrowRight width={16} height={16} />
-                                            </div>
-                                        ) : shouldShowBindPrompt ? (
-                                            <div onClick={handleBindInviteCode} className="px-10 w-fit h-full flex items-center justify-end gap-1 cursor-pointer hover:bg-gradient-to-r from-[#17D1B2] to-[#4C1F99] ">
-                                                <span className="text-sub font-[400] uppercase text-sm">BIND INVITE CODE</span>
-                                                <ArrowRight width={16} height={16} />
-                                            </div>
-                                        ) : (
-                                            <ConnectInfo />
-                                        )
-                                    ) : (
-                                        <div onClick={handleConnect} className="px-10 w-fit h-full flex items-center justify-end gap-1 cursor-pointer hover:bg-gradient-to-r from-[#17D1B2] to-[#4C1F99] ">
-                                            <span className="text-sub font-[400] uppercase text-sm">SIGN IN</span>
-                                            <ArrowRight width={16} height={16} />
-                                        </div>
-                                    )}
-                                </div>
-                            </div>
-                        </GradientBorderCard>
-                    </div>
-                </div>,
-                document.body
-            )
-        }
-        {/* <div className="h-[8rem]"></div> */}
-    </>;
+      <>
+        {createPortal(
+          <div className="hidden lg:block main-container fixed top-0 z-[11] h-[8rem] w-full left-1/2 -translate-x-1/2">
+            <div className="relative py-6">
+              <GradientBorderCard className="h-20 flex items-center backdrop-blur bg-[#090A09B2]">
+                <div className="w-full h-full flex justify-between items-center">
+                  <div className="flex items-center h-full flex-1">
+                    <Link to={"/"}>
+                      <img
+                        src={getImageUrl("@/assets/images/logo/cysic.svg")}
+                        className="flex-1 max-w-[11.25rem]"
+                      />
+                    </Link>
+
+                    {currentNavs.map((nav: any) => (
+                      <GradientNavDropdown
+                        className="flex-1 max-w-[11.25rem] text-center"
+                        key={nav.content}
+                        item={nav}
+                      />
+                    ))}
+                  </div>
+                  <div className="h-full flex items-center justify-end w-[26.75rem]">
+                    {!hasTryToConnectedOnce ? (
+                      <Spinner className="px-10" />
+                    ) : walletAddress ? (
+                      !isSigned ? (
+                        <div
+                          onClick={handleLoginPersonalMessage}
+                          className="px-10 w-fit h-full flex items-center justify-end gap-1 cursor-pointer hover:bg-gradient-to-r from-[#17D1B2] to-[#4C1F99] "
+                        >
+                          <span className="text-sub font-[400] uppercase text-sm">
+                            SIGN MESSAGE
+                          </span>
+                          <ArrowRight width={16} height={16} />
+                        </div>
+                      ) : shouldShowBindPrompt ? (
+                        <div
+                          onClick={handleBindInviteCode}
+                          className="px-10 w-fit h-full flex items-center justify-end gap-1 cursor-pointer hover:bg-gradient-to-r from-[#17D1B2] to-[#4C1F99] "
+                        >
+                          <span className="text-sub font-[400] uppercase text-sm">
+                            BIND INVITE CODE
+                          </span>
+                          <ArrowRight width={16} height={16} />
+                        </div>
+                      ) : (
+                        <ConnectInfo />
+                      )
+                    ) : (
+                      <div
+                        onClick={handleConnect}
+                        className="px-10 w-fit h-full flex items-center justify-end gap-1 cursor-pointer hover:bg-gradient-to-r from-[#17D1B2] to-[#4C1F99] "
+                      >
+                        <span className="text-sub font-[400] uppercase text-sm">
+                          SIGN IN
+                        </span>
+                        <ArrowRight width={16} height={16} />
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </GradientBorderCard>
+            </div>
+          </div>,
+          document.body
+        )}
+      </>
+    </>
+  );
 }
