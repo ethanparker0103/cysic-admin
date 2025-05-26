79c87ae006caab0ab25d847b90faac7951ea5a48
ethanparker <ethanparker0103@outlook.com>
2025-05-26 20:24:43 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 // https://vitejs.dev/config/
 export default defineConfig({
   esbuild: {
-    drop: ['console', 'debugger'],
+    // drop: ['console', 'debugger'],
   },
   assetsInclude: ['**/*.wasm'],
   plugins: [react()],
